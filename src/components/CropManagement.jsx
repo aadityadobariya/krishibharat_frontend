f1e1b93df7e34e7ae4c28ae127192e872d43be13
aadityadobariya <aadityadobariya@gmail.com>
2025-02-08 10:39:20 +0530

    add refetch in crops management

--- src/components/CropManagement.jsx
+++ src/components/CropManagement.jsx
@@ -1,6 +1,6 @@
 import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
 import Cookies from "js-cookie";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   useAddCropDetailsMutation,
@@ -58,6 +58,10 @@ const CropManagement = () => {
     setShowModal(true);
     setViewCrop(null);
   };
+
+  useEffect(() => {
+    refetch();
+  });
 
   const handleSubmit = async () => {
     const cropData = {
